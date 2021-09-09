28d71274651a5e3c95af3214cc5d2ff95b19282f
shedzafyah <shedzafyah2@gmail.com>
2021-09-09 14:22:48 +0200

    Initial Commit

---
+++ src/zw/co/afrosoft/Main.java
@@ -0,0 +1,27 @@
+package zw.co.afrosoft;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /* 1 Stream Integer
+
+                IntStream.range(1,10)
+                .skip(2)
+                .forEach(x-> System.out.println(x));
+
+            2 Stream Summing
+                 System.out.println(
+                    IntStream.range(1,5)
+                    .skip(2)
+                    .sum());
+
+            3. Stream.of Sorted and findFirst
+         */
+
+
+
+    }
+}
