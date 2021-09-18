294c4f023e8c76f562db31a45449f5824d70b1b9
shelton <shedzafyah2@gmail.com>
2021-09-18 19:21:11 +0200

    combination of filter,map & sort

--- src/zw/co/afrosoft/map/MapDemo1.java
+++ src/zw/co/afrosoft/map/MapDemo1.java
@@ -5,7 +5,62 @@ import java.util.List;
 
 public class MapDemo1 {
     public static void main(String[] args) {
-        List<String> vehiclesModels = Arrays.asList("madza","toyota","ranger","nissan","benz","bmw");
-        vehiclesModels.stream().filter(s->s.length()>2&& s.length()<5).map(v->v.toUpperCase()).sorted().forEach(g-> System.out.println(g));
+        List<Employee> employeeList = Arrays.asList(
+                new Employee("shelton",23,100),
+                new Employee("owen",23,100),
+                new Employee("kuda",14,500),
+                new Employee("panashe",35,700),
+                new Employee("nyasha",10,200),
+                new Employee("romeo",20,400),
+                new Employee("ashton",15,600)
+
+        );
+
+        employeeList.stream().filter(employee -> employee.getAge()<20).map(employee -> employee.getName()).sorted().forEach(System.out::println);
+    }
+}
+
+class Employee{
+    String name;
+    int age;
+    int salary;
+
+    public Employee(String name, int age, int salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", salary=" + salary +
+                '}';
     }
 }
