ddefa53f03b44716aad015de100283ae6492cb0d
shedzafyah <shedzafyah2@gmail.com>
2021-09-21 13:45:47 +0200

    anyMatch,allMatch,noneMatch

--- src/zw/co/afrosoft/map/MapDemo1.java
+++ src/zw/co/afrosoft/map/MapDemo1.java
@@ -6,17 +6,29 @@ import java.util.List;
 public class MapDemo1 {
     public static void main(String[] args) {
         List<Employee> employeeList = Arrays.asList(
-                new Employee("shelton",23,100),
-                new Employee("owen",23,100),
-                new Employee("owen",14,500),
-                new Employee("owen",35,700),
-                new Employee("nyasha",20,200),
-                new Employee("romeo",20,400),
-                new Employee("ashton",15,600)
+                new Employee("shelton",23,100,"Admin"),
+                new Employee("owen",23,100,"User"),
+                new Employee("kuda",14,500,"Admin"),
+                new Employee("panashe",35,700,"User"),
+                new Employee("nyasha",10,200,"Admin"),
+                new Employee("romeo",20,400,"User"),
+                new Employee("ashton",15,600,"User")
 
         );
 
-        employeeList.stream().filter(employee -> employee.getAge()>10).map(employee -> employee.getName()).sorted().distinct().forEach(System.out::println);
+        employeeList.stream().filter(employee -> employee.getAge()<20).map(employee -> employee.getName()).sorted().forEach(System.out::println);
+
+        //using anyMatch
+        boolean any = employeeList.stream().anyMatch(employee -> employee.getAccount().matches("Admin"));
+        //nonMatch
+         boolean none =employeeList.stream().noneMatch(employee -> employee.getAccount().matches("User"));
+        //allMatch
+        boolean all =employeeList.stream().allMatch(employee -> employee.getAccount().matches("Admin"));
+
+        System.out.println(all);
+        System.out.println(any);
+        System.out.println(none);
+
     }
 }
 
@@ -24,11 +36,13 @@ class Employee{
     String name;
     int age;
     int salary;
+    String account;
 
-    public Employee(String name, int age, int salary) {
+    public Employee(String name, int age, int salary, String account) {
         this.name = name;
         this.age = age;
         this.salary = salary;
+        this.account = account;
     }
 
     public String getName() {
@@ -55,12 +69,21 @@ class Employee{
         this.salary = salary;
     }
 
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
     @Override
     public String toString() {
         return "Employee{" +
                 "name='" + name + '\'' +
-                ", age='" + age + '\'' +
+                ", age=" + age +
                 ", salary=" + salary +
+                ", account='" + account + '\'' +
                 '}';
     }
 }
